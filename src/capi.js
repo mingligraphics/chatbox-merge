36ea332b3b9eda4fb5a83e6d6b4275f909424393
MING LI <ming.li@wsj.com>
2025-07-29 11:15:47 -0400

    capi

--- src/capi.js
+++ src/capi.js
@@ -1,8 +1,17 @@
 // docs: https://github.dowjones.net/newsroom-innovation/capi-generator
-const { CapiGenerator } = require('capi-generator');
+const { CapiGenerator, Image } = require('capi-generator');
 
 module.exports = async function () {
   const cg = new CapiGenerator();
+  const image = new Image({
+    photoUrl: 'https://www.wsj.com/ai2html/41505aa6-b531-47d2-a619-fcf703e377fa/fallback_classic.jpg',
+    width: 600,
+    height: 1358,
+    alternate_text: '',
+    credit: '',
+    caption: '',
+  });
+  cg.addToBody(image);
 
   return cg;
 };
